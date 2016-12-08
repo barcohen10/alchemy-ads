daebd3a7b8e43cb315038705e76ad69dc19735ea
cohenba <bar.cohen@hpe.com>
2016-12-08 15:22:03 +0200

    first-commit

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	api_key: '4bf4407b04bbfeb82cc3bdfc715be1c34a3e19f3',
+
+	parameters: {
+	  extract: 'entities, keywords, concepts, taxonomy',
+	  sentiment: 1,
+	  maxRetrieve: 1
+	},
+
+	dbName: 'ads',
+
+	credentialsDB: {
+	"username": "b4959dc0-7080-4608-9ac7-153fd92497e1-bluemix",
+  "password": "3d6da7f39f8eb2f6eeb372617791fb9abd68a2b57a4fd6a277bfbc81c9c66017",
+  "host": "b4959dc0-7080-4608-9ac7-153fd92497e1-bluemix.cloudant.com",
+  "port": 443,
+  "url": "https://b4959dc0-7080-4608-9ac7-153fd92497e1-bluemix:3d6da7f39f8eb2f6eeb372617791fb9abd68a2b57a4fd6a277bfbc81c9c66017@b4959dc0-7080-4608-9ac7-153fd92497e1-bluemix.cloudant.com"
+}
+	
+}
